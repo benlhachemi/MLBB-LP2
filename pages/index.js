862b9447f781bd54d332b862c539e7211d591814
SOUHAIL BEN-LHACHEMI <souhailbenlhachemi1999@gmail.com>
2021-12-21 15:54:40 +0100

    image compressing

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
 				<meta name="description" content="free mobile legends diamonds for all mobile legends germs and fans, diamonds gratis" />
 				<meta name="keywords"    content="mobile legends, diamonds, mobile legends diamonds, moontoon, mobile legends gratis, mobile legends diamonds gratis"/>
 				<meta name="viewport"    content="width=device-width initial-scale=1.0 user-scalable=no"/>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="https://i.imgur.com/cNNizpw.png" />
 			</Head>
 			
 			<main className='w-full min-h-screen pb-10 bg-[url(/images/mobile-legends-diamonds-gratis.jpg)] bg-cover bg-center'>
@@ -35,7 +35,7 @@ export default function Home() {
 				<Navbar active={1}/>
 
 				{/* L O G O */}
-				<img src="/images/mobile-legends-diamonds-legends-logo.png" alt="Free Diamonds for all mobile legends gamers" className='cssanimation pushReleaseFrom [animation-delay:0.2s] mx-auto md:w-96 mb-10 sm:w-72 w-72'/>
+				<img src="https://i.imgur.com/cNNizpw.png" alt="Free Diamonds for all mobile legends gamers" className='cssanimation pushReleaseFrom [animation-delay:0.2s] mx-auto md:w-96 mb-10 sm:w-72 w-72'/>
 
 				{/* S T E P P E R */}
 				<main className="relative py-3 px-3 cssanimation blurInTop [animation-delay:0.8s] w-5/6 md:w-2/6 bg-black border-[1px] bg-opacity-50 backdrop-blur-sm shadow-lg border-white mx-auto rounded-lg text-white text-center">
